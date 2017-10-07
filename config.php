ff28ef2117e8f698423709fd45e68e5ad52ed7bc
crmserver <32027772+crmserver@users.noreply.github.com>
2017-10-07 17:50:59 +0800

    Update config.php

--- config.php
+++ config.php
@@ -14,6 +14,7 @@ function config($key = '')
             'about-us' => 'About Us',
             'products' => 'Products',
             'contact' => 'Contact',
+            'test' => 'Test',
         ],
         'template_path' => 'template',
         'content_path' => 'content',
