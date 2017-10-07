eeb4dc97f1046363b8e6d22b2291db9c5ed3d70b
crmserver <32027772+crmserver@users.noreply.github.com>
2017-10-07 17:49:52 +0800

    Add files via upload

---
+++ content/test.php
@@ -0,0 +1,3 @@
+<p><a href="content/show_article.php?id=124">unPretty url</a></p>
+<p><a href="articles/124">Pretty article url not working</a></p>
+<p><a href="articles/124.html">Pretty article url not working</a></p>
